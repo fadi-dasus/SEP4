2a75ec91bf49540cb1c4e87782bafb21001574f3
AlexZorin1994 <224493@via.dk>
2019-04-04 11:56:31 +0200

    Added models

---
+++ Android/src/app/src/main/java/com/example/legalizeco2/model/Humidity.java
@@ -0,0 +1,23 @@
+package com.example.legalizeco2.model;
+
+public class Humidity {
+
+    private float HuminPercentage;
+    private int HID;
+
+    public float getHuminPercentage() {
+        return HuminPercentage;
+    }
+
+    public void setHuminPercentage(float huminPercentage) {
+        HuminPercentage = huminPercentage;
+    }
+
+    public int getHID() {
+        return HID;
+    }
+
+    public void setHID(int HID) {
+        this.HID = HID;
+    }
+}
