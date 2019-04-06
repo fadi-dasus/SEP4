48c54bdffc037816fb6dfd6372f67fbf1dd9f9a3
fadi-dasus <31960592+fadi-dasus@users.noreply.github.com>
2019-04-06 11:27:54 +0200

    Code refactor

---
+++ Database/Code/Sep4/src/main/java/com/dataConnection/Sep4/SQL/dao/Co2Repository.java
@@ -0,0 +1,9 @@
+package com.dataConnection.Sep4.SQL.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.dataConnection.Sep4.SQL.model.Co2;
+
+public interface Co2Repository extends CrudRepository<Co2, Integer> {
+
+}
--- Database/Code/Sep4/src/test/java/com/dataConnection/Sep4/Sep4ApplicationTests.java
+++ Database/Code/Sep4/src/test/java/com/dataConnection/Sep4/Sep4ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.dataConnection.Sep4.dao.Co2Repository;
-import com.dataConnection.Sep4.model.Co2;
+import com.dataConnection.Sep4.SQL.dao.Co2Repository;
+import com.dataConnection.Sep4.SQL.model.Co2;
 import com.dataConnection.Sep4.mongo.Room;
 import com.dataConnection.Sep4.mongo.RoomRepository;
 
