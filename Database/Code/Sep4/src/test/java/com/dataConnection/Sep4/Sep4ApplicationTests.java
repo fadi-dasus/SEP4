3b658604fdaea7e4d771b48ad80d7fe9917aa617
fadi-dasus <31960592+fadi-dasus@users.noreply.github.com>
2019-04-07 12:04:51 +0200

    provide fine grained control over restructuring data written to mongo (and reading from mongo)

--- Database/Code/Sep4/src/test/java/com/dataConnection/Sep4/Sep4ApplicationTests.java
+++ Database/Code/Sep4/src/test/java/com/dataConnection/Sep4/Sep4ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.dataConnection.Sep4;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +12,6 @@ import com.dataConnection.Sep4.SQL.dao.Co2Repository;
 import com.dataConnection.Sep4.SQL.model.Co2;
 import com.dataConnection.Sep4.mongo.Room;
 import com.dataConnection.Sep4.mongo.RoomRepository;
-
-import ch.qos.logback.core.net.SyslogOutputStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,20 +25,32 @@ public class Sep4ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		co2.deleteAll();
 		co2.save(new Co2("14ppm"));
 		co2.save(new Co2("15ppm"));
 		co2.save(new Co2("16ppm"));
 		co2.findAll().forEach(System.out::println);
-		;
+		System.out.println("_______________________________");
 
 	}
 
 	@Test
 	public void contextLoadsMongo() {
-		rr.insert(new Room("10000"));
-
+//		rr.insert(new Room("100001"));
+		Room room = rr.findByRoomNumber("100001");
+		System.out.println("_______________________________");
+		System.out.println(room.toString());
+		System.out.println("_______________________________");
+		List<Room> roms = rr.findAll();
+		for (Room room2 : roms) {
+			System.out.println(" this is the room from the list ");
+			System.out.println(room2.toString());
+			System.out.println("_______________________________");
+		}
+		
+		
 		rr.findAll().forEach(System.out::println);
-		;
+		
 
 	}
 }
