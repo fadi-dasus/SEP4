8ce9adae47c69c76171e6f61146898cd6018c6cc
fadi-dasus <31960592+fadi-dasus@users.noreply.github.com>
2019-04-08 23:09:30 +0200

    Enable Scheduling version 0.1 (printing the time every 5 seconds )

--- Database/Code/Sep4 data extractor/src/main/java/com/dataConnection/Sep4/Sep4Application.java
+++ Database/Code/Sep4 data extractor/src/main/java/com/dataConnection/Sep4/Sep4Application.java
@@ -2,8 +2,10 @@ package com.dataConnection.Sep4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Sep4Application
 
 {
