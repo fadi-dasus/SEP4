bd6d1ad117d1853e4feacdc07d615bd8c3d1522d
R0b1S <robiss394@gmail.com>
2019-05-11 00:18:07 +0200

    rtos update

--- Embedded/FreeRTOS/MyFirstProject/mySemaphores.c
+++ Embedded/FreeRTOS/MyFirstProject/mySemaphores.c
@@ -1,15 +1,24 @@
 #include "mySemaphores.h"
 
 void semaphores_init() {
+	
+		cycleSemaphore = xSemaphoreCreateMutex();
+		if( cycleSemaphore == NULL )
+			cycleSemaphore = xSemaphoreCreateMutex();	// try one more time
+	
+		co2Semaphore = xSemaphoreCreateMutex();
+		if( co2Semaphore == NULL )
+			co2Semaphore = xSemaphoreCreateMutex();		// try one more time
+				
+		tempHumSemaphore = xSemaphoreCreateMutex();	
+		if( tempHumSemaphore == NULL )
+			tempHumSemaphore = xSemaphoreCreateMutex();	// try one more time
 		
-	/*do {*/
-		if(CO2Semaphore == NULL)
-			CO2Semaphore = xSemaphoreCreateMutex();
-		if(TempHumSemaphore == NULL)
-			TempHumSemaphore = xSemaphoreCreateMutex();
-		if(LoRaSemaphore == NULL)
-			LoRaSemaphore = xSemaphoreCreateMutex();
+		loraSemaphore = xSemaphoreCreateMutex();	
+		if( loraSemaphore == NULL )
+			loraSemaphore = xSemaphoreCreateMutex();	// try one more time
 		
-	/*} while(CO2Semaphore == NULL || TempHumSemaphore == NULL || LoRaSemaphore == NULL);*/
-
+		queueSemaphore = xSemaphoreCreateMutex();
+		if( queueSemaphore == NULL )
+			queueSemaphore = xSemaphoreCreateMutex();	// try one more time
 }
