332b377f13aed8926e6d23a5f8816c0dcbf07838
Yasin Issa Aden <267276@via.dk>
2019-05-13 16:27:29 +0200

    Update

--- Database/Code/Sep4 data extractor/src/main/java/com/dataConnection/Sep4/SQL/dao/Co2Repository.java
+++ Database/Code/Sep4 data extractor/src/main/java/com/dataConnection/Sep4/SQL/dao/Co2Repository.java
@@ -1,7 +1,9 @@
 package com.dataConnection.Sep4.SQL.dao;
 
+import java.sql.Timestamp;
 import java.util.List;
 
+import com.dataConnection.Sep4.SQL.model.Room;
 import org.springframework.data.repository.CrudRepository;
 
 import com.dataConnection.Sep4.SQL.model.Co2;
