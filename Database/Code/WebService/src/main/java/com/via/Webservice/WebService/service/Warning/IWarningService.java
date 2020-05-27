666fc5b9484259beac34bdd9ef124a768ec8e273
fadi-dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-05-27 18:14:20 +0200

    get and add users

--- Database/Code/WebService/src/main/java/com/via/Webservice/WebService/service/Warning/IWarningService.java
+++
@@ -1,8 +0,0 @@
-package com.via.Webservice.WebService.service.Warning;
-
-import com.via.Webservice.WebService.model.Room;
-import com.via.Webservice.WebService.model.Warning;
-
-public interface IWarningService {
-	public Iterable<Warning> getAllWarningByRoomId(Room room);
-}
